c8292b60f74cabb83ba288f5bbbaf958e9c805a9
eddy <eddyxu1213@126.com>
2016-12-27 20:49:14 +0800

    增加groovy测试

---
+++ groovyxiao/src/test/java/org/eddy/A.groovy
@@ -0,0 +1,18 @@
+package org.eddy
+
+/**
+ * Created by eddy on 2016/12/27.
+ */
+class A extends ArrayList<String> {
+    A(var1) {
+        super(var1 as int)
+    }
+
+    static def a() {
+        'a'
+    }
+
+    static String b() {
+        'b'
+    }
+}
---
+++ groovyxiao/src/test/java/org/eddy/test.groovy
@@ -0,0 +1,11 @@
+package org.eddy
+
+/**
+ * Created by eddy on 2016/12/27.
+ */
+A a = new A();
+println a.b()
+
+a.add(a.a())
+a.add(a.b())
+println a
