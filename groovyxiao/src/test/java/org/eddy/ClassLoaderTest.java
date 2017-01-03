b328b017d7837405a7b47b921094ff9f1eb48964
justice-eddy <eddy.xu@oceanwing.com>
2017-01-03 14:25:33 +0800

    修改测试用例

--- groovyxiao/src/test/java/org/eddy/ClassLoaderTest.java
+++ groovyxiao/src/test/java/org/eddy/ClassLoaderTest.java
@@ -2,13 +2,12 @@ package org.eddy;
 
 import org.eddy.classLoader.MyUrlClassLoader;
 import org.eddy.classLoader.Some;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
-import java.net.MalformedURLException;
+import java.lang.reflect.Method;
 import java.net.URL;
-import java.net.URLClassLoader;
+import java.util.Arrays;
 
 /**
  * Created by eddy on 17/1/3.
@@ -20,12 +19,15 @@ public class ClassLoaderTest {
         MyUrlClassLoader urlClassLoader = new MyUrlClassLoader(new URL[]{new File("/Users/eddy/xiao/groovyxiao/target/groovy.xiao-1.0-SNAPSHOT.jar").toURI().toURL()});
         Class some1 = urlClassLoader.loadClass("org.eddy.classLoader.Some");
         System.out.println(some1.getClassLoader());
-        test2((Some) some1.newInstance()); //  child 转成 parent
-        ((Some) some1.newInstance()).other(new Some()); // parent专程 child
+//        test2((Some) some1.newInstance()); //  child 转成 parent
+
+        //parent 转成 child
+        Method method = Arrays.stream(some1.getDeclaredMethods()).filter(s -> s.getName().equals("other")).findFirst().get();
+        method.invoke(some1.newInstance(), new Some());
     }
 
     public void test2(Some some) {
-        System.out.println(some.name("xuyi"));
+        System.out.println(some.name("test"));
     }
 
 
